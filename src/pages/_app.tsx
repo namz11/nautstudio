3d4022e417b2498d33b653cc074866f0cd764e1b
narmit <narmitmashruwala@gmail.com>
2022-06-11 17:41:12 +0530

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "@styles/globals.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Naut Studio - Namit Rajput</title>
+        <link rel="icon" href="/logo.jpeg" />
+
+        {/* TODO update list */}
+        <meta
+          name="keywords"
+          content="architecture, interior, design, interior design, naut, naut studio, naut studios, namit, namit rajput"
+        />
+        {/* TODO update description */}
+        <meta name="description" content="Naut Studio Pvt. Ltd." />
+        <meta name="author" content="Narmit Mashruwala" />
+      </Head>
+
+      <div id="container">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="wrapper">
+      <img src="/logo.jpeg" alt="Naut Studio logo" className="logo" />
+      <p>Naut Studio website coming soon!</p>
+    </main>
+  );
+}
